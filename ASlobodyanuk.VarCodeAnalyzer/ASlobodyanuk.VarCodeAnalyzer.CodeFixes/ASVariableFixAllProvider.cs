d8c812005aca00374ad7175d1ea2ce8821915a0e
Andrii Slobodianiuk <slobodyandev@gmail.com>
2022-06-17 17:08:56 +0200

    Fix processing of output directory files when fixing full solution

--- ASlobodyanuk.VarCodeAnalyzer/ASlobodyanuk.VarCodeAnalyzer.CodeFixes/ASVariableFixAllProvider.cs
+++ ASlobodyanuk.VarCodeAnalyzer/ASlobodyanuk.VarCodeAnalyzer.CodeFixes/ASVariableFixAllProvider.cs
@@ -2,6 +2,7 @@
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -18,21 +19,31 @@ namespace ASlobodyanuk.VarCodeAnalyzer
 
         public override IEnumerable<FixAllScope> GetSupportedFixAllScopes() => new List<FixAllScope>()
             {
+                FixAllScope.Document,
                 FixAllScope.Solution
             };
 
         public override Task<CodeAction> GetFixAsync(FixAllContext fixAllContext)
         {
+            if (fixAllContext.Scope == FixAllScope.Document)
+            {
+                return Task.FromResult(
+                            CodeAction.Create(
+                                title: CodeFixResources.CodeFixTitle,
+                                createChangedDocument: c => ProcessDocumentAsync(fixAllContext, fixAllContext.Document, c),
+                                equivalenceKey: nameof(CodeFixResources.CodeFixTitle)));
+            }
+
             return Task.FromResult(
-                    CodeAction.Create(
-                        title: CodeFixResources.CodeFixTitle,
-                        createChangedSolution: c => ChangeSolution(fixAllContext.Solution, fixAllContext, c),
-                        equivalenceKey: nameof(CodeFixResources.CodeFixTitle)));
+                        CodeAction.Create(
+                            title: CodeFixResources.CodeFixTitle,
+                            createChangedSolution: c => ChangeSolution(fixAllContext.Solution, fixAllContext, c),
+                            equivalenceKey: nameof(CodeFixResources.CodeFixTitle)));
         }
 
         private async Task<Document> ProcessDocumentAsync(FixAllContext context, Document document, CancellationToken cancellationToken)
         {
-            var diagnostics = (await context.GetDocumentDiagnosticsAsync(document))
+            var diagnostics = (await context.WithCancellationToken(cancellationToken).GetDocumentDiagnosticsAsync(document))
                                         .Where(x => x.Id == VariableCodeAnalyzer.DiagnosticId);
 
             return await ASCodeFixService.ApplyDiagnosticFixesAsync(diagnostics, document, cancellationToken);
@@ -41,7 +52,7 @@ namespace ASlobodyanuk.VarCodeAnalyzer
         private async Task<Solution> ChangeSolution(Solution solution, FixAllContext context, CancellationToken cancellationToken)
         {
             var documents = solution.Projects.SelectMany(x => x.Documents)
-                                            .Where(x => Path.GetExtension(x.FilePath) == ".cs")
+                                            .Where(x => Path.GetExtension(x.FilePath) == ".cs" && IsProjectOutputDocument(x) == false)
                                             .ToList();
 
             var updatedSolution = solution;
@@ -49,7 +60,7 @@ namespace ASlobodyanuk.VarCodeAnalyzer
             var tasks = documents.Select(async (document) =>
             {
                 var updatedDocument = await ProcessDocumentAsync(context, document, cancellationToken);
-                var syntaxRoot = await updatedDocument.GetSyntaxRootAsync();
+                var syntaxRoot = await updatedDocument.GetSyntaxRootAsync(cancellationToken);
 
                 return (Document: updatedDocument, SyntaxRoot: syntaxRoot);
             });
@@ -59,6 +70,15 @@ namespace ASlobodyanuk.VarCodeAnalyzer
                 updatedSolution = updatedSolution.WithDocumentSyntaxRoot(document.Document.Id, document.SyntaxRoot);
 
             return updatedSolution;
+        }
+
+        private bool IsProjectOutputDocument(Document document)
+        {
+            var projectFolder = Directory.GetParent(document.Project.FilePath);
+            var objPath = projectFolder + "\\obj";
+            var binPath = projectFolder + "\\bin";
+
+            return document.FilePath.Contains(objPath) || document.FilePath.Contains(binPath);
         }
     }
 }
